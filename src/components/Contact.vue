d55da018cca09858299371c7943515525d79b320
lhefdh <lhefdh@gmail.com>
2024-05-12 15:43:30 +0200

    Page de contact

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -1,3 +1,24 @@
 <template>
-    
+    <div>
+        <div>
+            <p>N'hésitez Pas</p>
+            <h1>Contactez Moi</h1>
+            <div>
+                <div>
+                    <img 
+                    src="../assets/email.png" 
+                    alt="Email icon"
+                    > 
+                    <p><a href="mailto:lhefdh@gmail.com">lhefdh@gmail.com</a></p>
+                </div>
+                <div>
+                    <img 
+                    src="../assets/linkedin.png" 
+                    alt="Linkedin icon"
+                    > 
+                    <p><a href="https://linkedin.com">Linkedin Profile</a></p>
+                </div>
+            </div>
+        </div>
+    </div>       
 </template>
