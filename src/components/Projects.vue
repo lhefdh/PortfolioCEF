f28c6c4136eeb71ed670d05941fd5ae016d701bf
lhefdh <lhefdh@gmail.com>
2024-05-12 15:36:02 +0200

    Mes Projets

---
+++ src/components/Projects.vue
@@ -0,0 +1,47 @@
+<template>
+    <div>
+        <p>Decouvrez mes récents</p>
+        <h1>Projets</h1>
+        <div>
+            <div>
+                <div>
+                    <div>
+                        <img 
+                        src="../assets/cv-icon.png" 
+                        alt="Project 1"
+                        >
+                    </div>
+                    <h2 class="project-title">Curriculum-vitae</h2>
+                    <div>
+                        <button onclick="location.href='https://github.com'">Github</button>
+                    </div>
+                </div>
+                <div>
+                    <div>
+                        <img 
+                        src="../assets/dynamique.png" 
+                        alt="Project 2"
+                        >
+                    </div>
+                    <h2>Page Dynamique</h2>
+                    <div>
+                        <button onclick="location.href='https://github.com'">Github</button>
+                    </div>
+                </div>
+                <div>
+                    <div>
+                        <img 
+                        src="../assets/portfolio.png" 
+                        alt="Project 3"
+                        >
+                    </div>
+                    <h2>Portfolio</h2>
+                    <div>
+                        <button onclick="location.href='https://github.com'">Github</button>
+                    </div>
+                </div>
+            </div>
+            
+        </div>
+    </div>
+</template>
