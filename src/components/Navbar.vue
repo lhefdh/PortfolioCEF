d06e4d9064fc45ef040f4696daadae6eda9ead9b
lhefdh <lhefdh@gmail.com>
2024-05-12 14:19:45 +0200

    routing

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,3 +1,31 @@
 <template>
-    
+    <div>
+        <nav>
+            <div class="logo">Mohamed Fadel CHEIKH SAAD BOUH</div>
+            <ul class="nav-links">
+                <li><router-link to="/">Accueil</router-link></li>
+                <li><router-link to="/aboutme">Profil</router-link></li>
+                <li><router-link to="/experience">Connaissances</router-link></li>
+                <li><router-link to="/projects">Projets</router-link></li>
+                <li><router-link to="/contact">Contact</router-link></li>
+            </ul>   
+        </nav>
+        
+        <router-view></router-view>
+      
+        <footer>
+            <nav>
+                <div class="nav-links-container">
+                    <ul class="nav-links">
+                        <li><router-link to="/">Accueil</router-link></li>
+                        <li><router-link to="/aboutme">Profil</router-link></li>
+                        <li><router-link to="/experience">Connaissances</router-link></li>
+                        <li><router-link to="/projects">Projets</router-link></li>
+                        <li><router-link to="/contact">Contact</router-link></li>
+                    </ul>  
+                </div>
+            </nav>
+            <p>Copyright &#169; 2024</p>
+        </footer>
+    </div>
 </template>
