8e7e49bf4435f5aab0ff90b4cdde4345bde4d09f
lhefdh <lhefdh@gmail.com>
2024-05-12 15:22:42 +0200

    Connaissances

--- src/components/Experience.vue
+++ src/components/Experience.vue
@@ -1,3 +1,120 @@
 <template>
-    
+        <div>
+        <p>Explorez</p>
+        <h1>Mes Outils</h1>
+        <div>
+          <div>
+            <div>
+              <h2>Frontend</h2>
+              <div>
+                <article>
+                  <img 
+                  src="../assets/checkmark.png" 
+                  alt="Experience icon">
+                  <div>
+                    <h3>HTML</h3>
+                    <P>Maitrisé</P>
+                  </div>
+                </article>
+                <article>
+                  <img 
+                  src="../assets/checkmark.png" 
+                  alt="Experience icon"
+                >
+                  <div>
+                    <h3>CSS</h3>
+                    <P>Maitrisé</P>
+                  </div>
+                </article>
+                <article>
+                  <img 
+                  src="../assets/checkmark.png" 
+                  alt="Experience icon"
+                  >
+                  <div>
+                    <h3>SASS</h3>
+                    <P>Maitrisé</P>
+                  </div>
+                </article>
+                <article>
+                  <img 
+                  src="../assets/checkmark.png" 
+                  alt="Experience icon"
+                  >
+                  <div>
+                    <h3>JavaScript</h3>
+                    <P>Maitrisé</P>
+                  </div>
+                </article>
+                <article>
+                  <img 
+                  src="../assets/checkmark.png" 
+                  alt="Experience icon"
+                  >
+                  <div>
+                    <h3>TypeScript</h3>
+                    <P>Débutant</P>
+                  </div>
+                </article>
+                <article>
+                  <img 
+                  src="../assets/checkmark.png" 
+                  alt="Experience icon"
+                  >
+                  <div>
+                    <h3>Material UI</h3>
+                    <P>Débutant</P>
+                  </div>
+                </article>
+              </div>
+            </div>
+            <div>
+              <h2>Backend</h2>
+              <div>
+                <article>
+                  <img 
+                  src="../assets/checkmark.png" 
+                  alt="Experience icon"
+                 >
+                  <div>
+                    <h3>PostgreSQL</h3>
+                    <P>Débutant</P>
+                  </div>
+                </article>
+                <article>
+                  <img 
+                  src="../assets/checkmark.png" 
+                  alt="Experience icon"
+                  >
+                  <div>
+                    <h3>Node JS</h3>
+                    <P>Intermédiaire</P>
+                  </div>
+                </article>
+                <article>
+                  <img 
+                  src="../assets/checkmark.png" 
+                  alt="Experience icon"
+                  >
+                  <div>
+                    <h3>Express JS</h3>
+                    <P>Intermédiaire</P>
+                  </div>
+                </article>
+                <article>
+                  <img 
+                  src="../assets/checkmark.png" 
+                  alt="Experience icon"
+                  >
+                  <div>
+                    <h3>Git</h3>
+                    <P>Intermédiaire</P>
+                  </div>
+                </article>
+              </div>
+            </div>
+          </div>
+        </div>
+    </div>
+
 </template>
