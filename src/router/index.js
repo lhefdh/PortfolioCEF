f9e48ae03b242c46bd5d2f38ee221d7e16f436a1
lhefdh <lhefdh@gmail.com>
2024-05-12 14:00:45 +0200

    starting

---
+++ src/router/index.js
@@ -0,0 +1,39 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import Home from '../components/Home.vue'
+import Aboutme from '../components/Aboutme.vue'
+import Experience from '../components/Experience.vue'
+import Projects from '../components/Projects.vue'
+import Contact from '../components/Contact.vue'
+
+const routes = [
+    {
+        path: '/',
+        name: 'Home',
+        component: Home
+      },
+      {
+        path: '/aboutme',
+        name: 'Aboutme',
+        component: Aboutme
+      },
+      {
+        path: '/experience',
+        name: 'Experience',
+        component: Experience
+      },
+      {
+        path: '/projects',
+        name: 'Projects',
+        component: Projects
+      },
+      {
+        path: '/contact',
+        name: 'Contact',
+        component: Contact
+      },
+]
+const router = createRouter({
+    history: createWebHistory(),
+    routes
+})
+export default router
