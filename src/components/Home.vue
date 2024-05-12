0d1d18a49e69adc219c443536ba08417eb89e359
lhefdh <lhefdh@gmail.com>
2024-05-12 14:53:38 +0200

    page d'accueil

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,3 +1,48 @@
 <template>
-    
+    <div>
+          
+        <div>
+            <img src="../assets/pdp.png" alt="Mohamed Fadel profile picture">
+        </div>
+        <div>
+            <p>Salut, je m'appelle</p>
+            <h1> Mohamed Fadel <br> CHEIKH SAAD BOUH </h1>
+            <br>
+            <P>Développeur Full-Stack</P>
+            <div> 
+                <button onclick="window.open('../assets/Curriculum-vitae.pdf')">    
+                        Mon CV
+                </button>
+                <button>
+                    <router-link to="/contact">
+                        Contact
+                    </router-link>
+                </button>
+            </div>
+            <div>
+                <a href="https://linkedin.com/">
+                    <img src="../assets/linkedin.png" 
+                    alt="ma Page LinkedIn"/>
+                </a>
+                <a href="https://github.com/">
+                    <img src="../assets/github.png" 
+                    alt="Mon GitHub"/>
+                </a> 
+            </div>   
+        </div>
+        
+    </div>
 </template>
+
+<script>
+export default {
+  name: 'Home',
+  data() {
+    return {
+       
+      }
+    }
+    
+  }
+
+</script>
