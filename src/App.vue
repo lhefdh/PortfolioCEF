e57911939514970e3177205f8299b0191fd8c5da
lhefdh <lhefdh@gmail.com>
2024-05-19 14:47:48 +0200

    modal et fermeture de modal

--- src/App.vue
+++ src/App.vue
@@ -6,10 +6,21 @@
 
 <script>
 import Navbar from './components/Navbar.vue'
+import Home from './components/Home.vue'
+import Aboutme from './components/Aboutme.vue'
+import Experience from './components/Experience.vue'
+import Projects from './components/Projects.vue'
+import Contact from './components/Contact.vue'
+
 export default {
   name:'App',
   components: {
-    Navbar
+    Navbar,
+    Home,
+    Aboutme,
+    Experience,
+    Projects,
+    Contact
   }
 }
 </script>
