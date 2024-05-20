2827679b5893de5f38ec37694b3fa7b9d965e098
lhefdh <lhefdh@gmail.com>
2024-05-20 16:49:26 +0200

    one function instead of three using modal inside v-for

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -4,96 +4,85 @@
         <h1 class="title">Projets</h1>
         <div class="experience-details-container">
             <div class="about-container">
-                <div class="details-container color-container">
+                <div v-for="item in myProjects" :key="item.id" class="details-container color-container">
                     <div class="article-container">
-                        <img 
-                        src="../assets/cv-icon.png" 
-                        alt="CV CEF"
-                        class="project-img"
-                        >
+                        <img :src=item.imgSrc :alt=item.imgAlt class="project-img">
                     </div>
-                    <h2 class="project-title">Curriculum-vitae</h2>
+                    <h2 class="project-title">{{item.title}}</h2>
                     <div class="btn-container">
-                        <button class="btn" onclick="location.href='https://github.com/lhefdh/cv'">Github</button>
-                        <button class="btn" @click="modalOpen = !modalOpen;addProjectData1();" >Détails</button>
+                        <button class="btn" @click="window.open('{{item.repositoryLink}}')">Github</button>
+                        <button class="btn" @click="modalOpen = !modalOpen;addProjectData(item.id);" >Détails</button>
                     </div>
-                </div>
-                <div class="details-container color-container">
-                    <div class="article-container">
-                        <img 
-                        src="../assets/dynamique.png" 
-                        alt="Page Dynamique CEF"
-                        class="project-img"
-                        >
-                    </div>
-                    <h2 class="project-title">Page Dynamique</h2>
-                    <div class="btn-container">
-                        <button class="btn" onclick="location.href='https://github.com/lhefdh/page-dynamique'">Github</button>
-                        <button class="btn" @click="modalOpen = !modalOpen;addProjectData2();" >Détails</button>
-                    </div>
-                </div>
-                <div class="details-container color-container">
-                    <div class="article-container">
-                        <img 
-                        src="../assets/portfolio.png" 
-                        alt="Portfolio CEF"
-                        class="project-img"
-                        >
-                    </div>
-                    <h2 class="project-title">Portfolio</h2>
-                    <div class="btn-container">
-                        <button class="btn" onclick="location.href='https://github.com/lhefdh/PortfolioCEF'">Github</button>
-                        <button class="btn" @click="modalOpen = !modalOpen;addProjectData3();" >Détails</button>
-                    </div>
-                </div>
+                    <div class="modal-bg" :class="{ 'open' : modalOpen }" @click="modalOpen = !modalOpen">
+                        <div class="modal" @click="modalOpen = !modalOpen">
+                            <label for="title">Titre: {{modalData.title}} </label>
+                            <label for="date-of-creation">Date de création: {{modalData.date}} </label>
+                            <label for="tools">Technologies utilisées: {{modalData.tools}}</label>
+                            <label for="GitHub-link">Consulter <a :href=modalData.repositoryLink>le repository GitHub</a></label>
+                            <button class="btn modal-btn" @click="modalOpen = !modalOpen">Fermer</button>
+                        </div>
+                    </div>  
+                </div>  
             </div>
         </div>
-        <div class="modal-bg" :class="{ 'open' : modalOpen }" @click="modalOpen = !modalOpen">
-            <div class="modal" @click="modalOpen = !modalOpen">
-                <label for="title">Titre: {{myproject.name}} </label>
-                <label for="date-of-creation">Date de création: {{ myproject.date }} </label>
-                <label for="tools">Technologies utilisées: {{myproject.skills}}</label>
-                <label for="GitHub-link">Consulter <a :href=myproject.repositoryLink>le repository GitHub</a></label>
-                <button class="btn modal-btn" @click="modalOpen = !modalOpen">Fermer</button>
-            </div>
-
-        </div>
+        
     </div>
 </template>
 
 <script>
+    import cvIcon from '../assets/cv-icon.png'
+    import pageDynamiqueIcon from '../assets/dynamique.png'
+    import portfolioIcon from '../assets/portfolio.png'
     export default {
     name: 'Projects',
     data() {
         return {
             modalOpen: false,
-            myproject: {
-                name:'',
+            modalData: {
+                title:'',
                 date: '',
-                skills: '',
+                tools: '',
                 repositoryLink: '',
-            }
+            },
+            myProjects: [
+                 {
+                    id: 1,
+                    title:'Curriculum-Vitae',
+                    date: '24 Novembre 2023',
+                    tools: 'HTML et CSS',
+                    repositoryLink: 'https://github.com/lhefdh/cv',
+                    imgSrc: cvIcon,
+                    imgAlt:'CV CEF',
+                },
+                 {
+                    id: 2,
+                    title:'Page dynamique',
+                    date: '3 Février 2024',
+                    tools: 'HTML, CSS et JavaScript',
+                    repositoryLink: 'https://github.com/lhefdh/page-dynamique',
+                    imgSrc: pageDynamiqueIcon,
+                    imgAlt:'Page Dynamique CEF',
+                }, 
+                 {
+                    id: 3,
+                    title:'Portfolio',
+                    date: '19 Mai 2023',
+                    tools: 'HTML, CSS et JavaScript',
+                    repositoryLink: 'https://github.com/lhefdh/PortfolioCEF',
+                    imgSrc: portfolioIcon,
+                    imgAlt:'Portfolio CEF',
+                }
+            ] 
         }
-     },
-     methods: {
-        addProjectData1() {
-            this.myproject.name= 'Curriculum-Vitae',
-            this.myproject.date= '24 Novembre 2023',
-            this.myproject.skills= 'HTML et CSS',
-            this.myproject.repositoryLink= "https://github.com/lhefdh/cv"
-        },
-        addProjectData2() {
-            this.myproject.name= 'Page dynamique',
-            this.myproject.date= '3 Février 2024',
-            this.myproject.skills= 'HTML, CSS et JavaScript',
-            this.myproject.repositoryLink= "https://github.com/lhefdh/page-dynamique"
-        },
-        addProjectData3() {
-            this.myproject.name= 'Portfolio',
-            this.myproject.date= '19 Mai 2023',
-            this.myproject.skills= 'HTML, CSS et JavaScript',
-            this.myproject.repositoryLink= "https://github.com/lhefdh/PortfolioCEF"
+    },
+    methods: {
+        addProjectData(b) {  
+            this.modalData.title = this.myProjects[b-1].title,
+            this.modalData.date = this.myProjects[b-1].date,
+            this.modalData.tools = this.myProjects[b-1].tools,
+            this.modalData.repositoryLink = this.myProjects[id-1].repositoryLink
         }
-     }
+    }
 }
+     
 </script>
